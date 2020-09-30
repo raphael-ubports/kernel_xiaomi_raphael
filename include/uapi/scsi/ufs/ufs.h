aosp-mirror/kernel_msm

scsi: ufs: add manual_gc in sysfs
This patch adds manual_gc control via sysfs entries.

 > cat /sys/devices/platform/soc/1d84000.ufshc/manual_gc
on
 > echo 1 > /sys/devices/platform/soc/1d84000.ufshc/manual_gc

Bug: 63264275
Bug: 68721792
Change-Id: I4e778f716eed3e5a076e5d831a3b653ba8cd8458
Signed-off-by: Jaegeuk Kim <jaegeuk@google.com>

include/uapi/scsi/ufs/ufs.h
@@ -1,7 +1,7 @@
 #ifndef UAPI_UFS_H_
 #define UAPI_UFS_H_
 
-#define MAX_QUERY_IDN	0x12
+#define MAX_QUERY_IDN	0x18
 
 /* Flag idn for Query Requests*/
 enum flag_idn {
@@ -14,6 +14,8 @@ enum flag_idn {
 	QUERY_FLAG_IDN_RESERVED2		= 0x07,
 	QUERY_FLAG_IDN_FPHYRESOURCEREMOVAL      = 0x08,
 	QUERY_FLAG_IDN_BUSY_RTC			= 0x09,
+	/* use one reserved bit */
+	QUERY_FLAG_IDN_MANUAL_GC_CONT		= 0x0E,
 };
 
 /* Attribute idn for Query requests */
@@ -36,6 +38,8 @@ enum attr_idn {
 	QUERY_ATTR_IDN_SECONDS_PASSED		= 0x0F,
 	QUERY_ATTR_IDN_CNTX_CONF		= 0x10,
 	QUERY_ATTR_IDN_CORR_PRG_BLK_NUM		= 0x11,
+	/* use one reserved bit */
+	QUERY_ATTR_IDN_MANUAL_GC_STATUS		= 0x17,
 };
 
 #define QUERY_ATTR_IDN_BOOT_LU_EN_MAX	0x02

raphael-ubports/kernel_xiaomi_raphael

Signed-off-by: UtsavBalar1231 <utsavbalar1231@gmail.com>

include/uapi/scsi/ufs/ufs.h
@@ -1,7 +1,7 @@
 #ifndef UAPI_UFS_H_
 #define UAPI_UFS_H_
 
-#define MAX_QUERY_IDN	0x12
+#define MAX_QUERY_IDN	0x18
 
 /* Flag idn for Query Requests*/
 enum flag_idn {
@@ -14,6 +14,8 @@ enum flag_idn {
 	QUERY_FLAG_IDN_RESERVED2		= 0x07,
 	QUERY_FLAG_IDN_FPHYRESOURCEREMOVAL      = 0x08,
 	QUERY_FLAG_IDN_BUSY_RTC			= 0x09,
+	/* use one reserved bit */
+	QUERY_FLAG_IDN_MANUAL_GC_CONT		= 0x0E,
 };
 
 /* Attribute idn for Query requests */
@@ -37,6 +39,8 @@ enum attr_idn {
 	QUERY_ATTR_IDN_CNTX_CONF		= 0x10,
 	QUERY_ATTR_IDN_CORR_PRG_BLK_NUM		= 0x11,
 	QUERY_ATTR_IDN_REF_CLK_GATING_WAIT_TIME	= 0x17,
+	/* use one reserved bit */
+	QUERY_ATTR_IDN_MANUAL_GC_STATUS		= 0x18,
 };
 
 #define QUERY_ATTR_IDN_REF_CLK_GATING_WAIT_TIME \